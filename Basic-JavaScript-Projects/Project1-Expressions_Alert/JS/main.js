a510f6ce81d73e7033834afebeb8b5af26090002
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-07 13:58:09 -0500

    initial commit

---
+++ Basic-JavaScript-Projects/Project1-Expressions_Alert/JS/main.js
@@ -0,0 +1,52 @@
+//ALERT WINDOWS POP-UP
+window.alert("BOOO!!!");
+
+//DISPLAYS IN THE ACTUAL DOCUMENT, Can add <br> in document.write. \n wasn't working
+document.write("HEY YOU!<br>");
+
+//SIMPLE VARIABLE
+var X = 340000;
+//DISPLAYING VARIABLE IN DOCUMENT
+document.write(X,"<br>");
+
+//STRING VARIABLE
+var myName = "Jeremy's son ";
+
+//STRING DISPLAYED IN POP-UP
+//window.alert(myName);
+
+//PRACTICE USING / TO ESCAPE
+var sentence1 ='Jeremiah\'s a very special child!';
+
+//DISPLAYING THE VARIABLE ABOVE
+document.write(sentence1,"<br>");
+
+//CONCATENATION ASSIGNMENT
+var addSentences ='Joshua loves his brother Zachariah loves him very much! ' + 'However, in Jeremiah\'s mind he loves Josh even more than Zach does. ' + 'I guess God\'s the only One who knows... *shrugs*';
+
+//DISPLAYING THE VARIABLE ABOVE
+document.write(addSentences,"<br>");
+
+//CONCATENATION + STRING ASSIGNMENT
+var fatherAndSons = myName + addSentences;
+
+//DISPLAYING THE VARIABLE ABOVE
+document.write(fatherAndSons,"<br>");
+
+//ASSIGNING MULTIPLE VARIABLES AT ONCE
+var a=5, b=10, c=17;
+
+//DISPLAYING THE VARIABLES ABOVE
+document.write(a,"<br>");
+
+//Expression
+var thirty = 15 + 15;
+
+//DISPLAYING THE EXPRESSION ABOVE
+document.write(thirty,"<br>");
+
+var Sent1 = "This is the beginning of the string, ", Sent2 = "and this the end of the string."
+
+document.write(Sent1 + Sent2,"<br>");
+
+
